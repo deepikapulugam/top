6fc0dd78f77a43107866ea87b89cc8b6a49d24fe
praveenb231 <praveen.burugu@votarytech.com>
2017-03-07 14:37:36 +0530

    main.c
    main changed

--- main.c
+++ main.c
@@ -3,14 +3,6 @@
 int main()
 {
 int a,b,c;
-  printf("i changed ur code"):
-printf("main");
-printf("starting program");
-printf("middle program");
-printf("middle1");
-printf("middle2");
-printf("middle3");
-printf("end");
-printf("end1");
+  
 return 0;
 }
